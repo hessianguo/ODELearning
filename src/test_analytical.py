1810fb71326e5c3eef6297ee72a9aa3d16cb5659
Haibo Li <lee12358@163.com>
2025-01-19 21:55:41 +1100

    Update test_analytical.py

--- src/test_analytical.py
+++ src/test_analytical.py
@@ -1,3 +1,5 @@
+#%%
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import savemat
@@ -18,22 +20,26 @@ ns_type = 2
 # generata data
 g = lambda x: np.cos(x)
 dg = lambda x: -np.sin(x)
-T = np.linspace(-0.5, 0.5, pts_num)
+tt = np.array([-np.pi, np.pi])
+T = np.linspace(tt[0], tt[1], pts_num)
 X = g(T)
+x0 = X[0]
 X_data = np.reshape(X, (-1, pts_num))
-X_ns = add_noise(X_data, nsr, ns_type)
+X_ns = add_noise(X_data[:,1:], nsr, ns_type)
+x0 = np.array([[x0]])
+X_ns = np.hstack((x0, X_ns))
 Dx = dg(T)
 T1 = T[1:]
 
 
 # Compute the derivative using finite difference method
-X_dot_fd = compute_fd(X_data, T)
+X_dot_fd = compute_fd(X_ns, T)
 err_fd = compute_l2norm(X_dot_fd-Dx, T)/compute_l2norm(Dx, T)
 
 # Compute the error of the derivative using VRKHS
 kernel_type='gauss'
-#X_dot, X_fit, lamb1 = denoise_vrkhs(T, X_ns, 1e-4, 'pre_select', kernel_type, (0.2,))
-X_dot_rkhs, X_fit, lamb1 = denoise_vrkhs(T, X_ns, None, 'auto', kernel_type, (0.2,))
+# X_dot_rkhs, X_fit, lamb1 = denoise_vrkhs(T, X_ns, 5e-2, 'pre_select', kernel_type, (0.4,))
+X_dot_rkhs, X_fit, lamb1 = denoise_vrkhs(T, X_ns, None, 'auto', kernel_type, (0.5,))
 err_rkhs = compute_l2norm(X_dot_rkhs-Dx[1:], T[1:])/compute_l2norm(Dx[1:], T[1:])
 
 # Compute the error using TV regularization
@@ -78,3 +84,4 @@ plt.legend()
 plt.title('Derivative, true and TVR fitting')
 plt.tight_layout()
 plt.show()
+# %%
